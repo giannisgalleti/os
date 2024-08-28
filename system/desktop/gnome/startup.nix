50a358d2028d7c0be60aea8c3cc391f1780698dd
giannisgalleti <giannispapanatsios@yahoo.gr>
2024-08-28 23:33:04 +0300

    Disable autostart of Signal and Steam

--- system/desktop/gnome/startup.nix
+++ system/desktop/gnome/startup.nix
@@ -25,30 +25,30 @@ in
       {
         ${username}.home.file = mkIf (cfg.desktop.gnome.enable && cfg.desktop.gnome.startupItems) {
           # Add signal to startup
-          ".config/autostart/signal-desktop.desktop" = mkIf (user != "work") {
-            text = ''
-              [Desktop Entry]
-              Exec=signal-desktop
-              Icon=signal
-              Name=Signal
-              StartupWMClass=signal
-              Terminal=false
-              Type=Application
-            '';
-          };
+          # ".config/autostart/signal-desktop.desktop" = mkIf (user != "work") {
+          #   text = ''
+          #     [Desktop Entry]
+          #     Exec=signal-desktop
+          #     Icon=signal
+          #     Name=Signal
+          #     StartupWMClass=signal
+          #     Terminal=false
+          #     Type=Application
+          #   '';
+          # };
 
           # Add steam to startup
-          ".config/autostart/steam.desktop" = mkIf (user != "work") {
-            text = ''
-              [Desktop Entry]
-              Exec=steam
-              Icon=steam
-              Name=Steam
-              StartupWMClass=steam
-              Terminal=false
-              Type=Application
-            '';
-          };
+          # ".config/autostart/steam.desktop" = mkIf (user != "work") {
+          #   text = ''
+          #     [Desktop Entry]
+          #     Exec=steam
+          #     Icon=steam
+          #     Name=Steam
+          #     StartupWMClass=steam
+          #     Terminal=false
+          #     Type=Application
+          #   '';
+          # };
         };
       }
     ) users;
